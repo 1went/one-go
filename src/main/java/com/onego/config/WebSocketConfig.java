08f07cf6fdf83d9d6c694dfbf375fcfaa5a2dc10
yiwentao <1403693680@qq.com>
2022-11-11 22:47:37 +0800

    add websocket

---
+++ src/main/java/com/onego/config/WebSocketConfig.java
@@ -0,0 +1,8 @@
+package com.onego.config;
+
+/**
+ * @author yiwt
+ * @Date 2022/11/11 19:53
+ */
+public class WebSocketConfig {
+}
---
+++ src/main/java/com/onego/server/WebSocketServer.java
@@ -0,0 +1,8 @@
+package com.onego.server;
+
+/**
+ * @author yiwt
+ * @Date 2022/11/11 19:54
+ */
+public class WebSocketServer {
+}
